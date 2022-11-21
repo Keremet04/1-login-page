3fe93107c17781ed47c3a0682231c3567673f46e
Keremet Dogdurbaeva <dogdurbaeva04@bk.ru>
2022-11-21 11:47:30 +0600

    Home work 3/1

--- 1 login page/ViewController.swift
+++ 1 login page/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
 
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var TextFieldName: UITextField!
+    
+    @IBOutlet weak var TextFieldPassword: UITextField!
+    
+
+    @IBOutlet weak var TextResult: UILabel!
+    
+    
+
+    var name = ""
+    var password = ""
+    
+    @IBAction func buttonSignIn(_ sender: Any) {
+    
+    
+        name = TextFieldName.text!
+        password = TextFieldPassword.text!
+        
+        if name.count >= 3{
+            if password.count >= 3{
+                TextResult.textColor = .green
+                TextResult.text = "Success"
+            }else{
+                
+                TextResult.text = "Not correct data"
+                TextResult.textColor = .red
+                
+            }
+            
+        }
+        func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        TextFieldName.delegate = self
+        TextFieldPassword.delegate = self
+        
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.TextFieldName.resignFirstResponder()
+        self.TextFieldPassword.resignFirstResponder()
+        return true
+    }
+    
+    
+    
+   
 
-
+   
+    
+    
 }
 
+}
